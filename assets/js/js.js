4b8568265d38193ebded9c31e8145eab0a9e2108
swechchhaparajuli <swechchha@gmail.com>
2018-11-27 13:52:12 -0800

    added ajax functionality for the crime data

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,34 @@
+
+
+var dangerStat = [];
+
+
+$.ajax({
+  url: "https://data.sfgov.org/resource/cuks-n6tp.json",
+  type: "GET",
+  data: {
+    "$limit" : 5000,
+    "$$app_token" : "Q6SHiWLggs1tj51Wp1Y9CagsX"
+  }
+}).done(function(data) {
+  //alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+  dangerData(data);
+  
+});
+
+
+function dangerData(data){
+  for (var i = 0; i<5000; i++){
+    if (data[i].category == "ASSAULT" || data[i].category == "LARCENY/THEFT" || data[i].category == "BURGLARY" || data[i].category == "ROBBERY" || data[i].category == "VEHICLE THEFT"){
+      dangerStat[i] = data[i];
+    }
+  }
+  console.log(dangerStat);
+}
+
+
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
